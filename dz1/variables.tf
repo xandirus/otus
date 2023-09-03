c758f756a6841c5bf66eec7a6ad13331e8a84449
”maxfatalis” <root@shlx-laptop.shlx.local>
2023-09-03 09:09:00 +0300

    edit1

--- dz1/variables.tf
+++ dz1/variables.tf
@@ -1,20 +1,20 @@
 variable "zone" {
   type = string
-  default = "ru-central1-a"
+  default = "ru-central1-a" # зона доступности, можно не менять
 
 }
 
 variable "cloud_id" {
   type = string
-  default = "b1gmrlsb9f70ac226au7"
+  default = "b1gmrlsb9f70ac226au7" # нужен свой cloud_id
 }
 
 variable "folder_id" {
   type = string
-  default = "b1gbso9k07p4sob6ogrd"
+  default = "b1gbso9k07p4sob6ogrd" # нужен свой folder_id
 }
 
 variable "image_id" {
   type = string
-  default = "fd879gb88170to70d38a"
+  default = "fd879gb88170to70d38a" # образ VM, можно не менять
 }
